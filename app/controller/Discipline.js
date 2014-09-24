461bb5d738e08d7f2c40472e2ca4828ed9924cd2
samueltcsantos <thiagsamuel@gmail.com>
2014-09-23 22:36:00 -0300

    issue #2, carragando disciplina da base de dados para grid.

--- app/controller/Discipline.js
+++ app/controller/Discipline.js
@@ -12,7 +12,18 @@ Ext.define('SysContest.controller.Discipline', {
     views : [
        'SysContest.view.discipline.DisciplinesGrid'
        //'SysContest.view.institution.InstitutionsForm'
-    ]
+    ],
 
+    init : function (application) {
+        this.control({
+          'disciplinesgrid' : {
+             render : this.onGridRender
+          }
+        });
+    },
+
+    onGridRender : function (grid, eOpts){
+      grid.getStore().load();
+    },
   
 });
