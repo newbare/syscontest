7ae43a23d5e71612cb6fe9e3e8d329433258ef1e
samueltcsantos <thiagsamuel@gmail.com>
2014-09-26 17:33:22 -0300

    atualizado arquivo para listar questoes, issue #5.

--- php/question/listQuestions.php
+++ php/question/listQuestions.php
@@ -11,13 +11,13 @@
 	$query = mysql_query($queryString) or die(mysql_error());
 
 	//faz um looping e cria um array com os campos da consulta
-	$exames = array();
-	while($exam = mysql_fetch_assoc($query)) {
-	    $exames[] = $exam;
+	$questions = array();
+	while($question = mysql_fetch_assoc($query)) {
+	    $questions[] = $question;
 	}
 
 	//consulta total de linhas na tabela
-	$queryTotal = mysql_query('SELECT count(*) as num FROM exam') or die(mysql_error());
+	$queryTotal = mysql_query('SELECT count(*) as num FROM question') or die(mysql_error());
 	$row = mysql_fetch_assoc($queryTotal);
 	$total = $row['num'];
 
@@ -25,5 +25,5 @@
 	echo json_encode(array(
 		"success" => mysql_errno() == 0,
 		"total" => $total,
-		"data" => $exames
+		"data" => $questions
 	));
