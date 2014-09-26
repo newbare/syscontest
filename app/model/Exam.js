1466c33c8819529e14b1ceda9743d0534dff5dec
samueltcsantos <thiagsamuel@gmail.com>
2014-09-26 13:17:32 -0300

    iniciada a issue #4 , criado o Model Exam.

---
+++ app/model/Exam.js
@@ -0,0 +1,15 @@
+Ext.define('SysContest.model.Exam', {
+	extend : 'Ext.data.Model',
+
+	fields : [
+			{name : 'idExam', type : 'int'},
+			{name : 'idInstitution' , type : 'int'},
+			{name : 'year', type : 'int'},
+			{name : 'role', type : 'string'},
+			{name : 'level', type : 'string'},
+			{name : 'organ', type : 'string'}
+	],
+
+	belongsTo:{model:'SysContest.model.Discipline', foreignKey:'idDiscipline'}
+
+});
