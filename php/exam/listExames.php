8110ed69df3ce07781e89a2c835ea03dcaea8e52
samueltcsantos <thiagsamuel@gmail.com>
2014-09-26 14:49:26 -0300

    listando provas do banco de dados exam, issue #4.

--- php/exam/listExames.php
+++ php/exam/listExames.php
@@ -5,21 +5,19 @@
   $start = $_REQUEST['start'];
   $limit = $_REQUEST['limit'];
 
-	$queryString = "SELECT * FROM discipline LIMIT $start,  $limit";
+	$queryString = "SELECT * FROM exam LIMIT $start,  $limit";
 
- 	//$queryString = "SELECT * FROM institution";
-
-	//consulta sql
+	//execute sql query
 	$query = mysql_query($queryString) or die(mysql_error());
 
 	//faz um looping e cria um array com os campos da consulta
-	$disciplines = array();
-	while($discipline = mysql_fetch_assoc($query)) {
-	    $disciplines[] = $discipline;
+	$exames = array();
+	while($exam = mysql_fetch_assoc($query)) {
+	    $exames[] = $exam;
 	}
 
 	//consulta total de linhas na tabela
-	$queryTotal = mysql_query('SELECT count(*) as num FROM discipline') or die(mysql_error());
+	$queryTotal = mysql_query('SELECT count(*) as num FROM exam') or die(mysql_error());
 	$row = mysql_fetch_assoc($queryTotal);
 	$total = $row['num'];
 
@@ -27,5 +25,5 @@
 	echo json_encode(array(
 		"success" => mysql_errno() == 0,
 		"total" => $total,
-		"data" => $disciplines
+		"data" => $exames
 	));
