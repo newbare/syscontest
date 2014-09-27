a546fc9dd33332f4d00e9eb5786e45e20ad260f9
samueltcsantos <thiagsamuel@gmail.com>
2014-09-27 01:49:58 -0300

    atualizado arquivos .php para update de provas. issue #4.

--- php/exam/updateExam.php
+++ php/exam/updateExam.php
@@ -7,19 +7,33 @@
 	//$data = json_decode(stripslashes($info));
 	$data = json_decode($info);
 
-	$name = $data->name;
-	$id = $data->idDiscipline;
+	$idExam = $data->idExam;
+	$idInstitution = $data->idInstitution;
+	$year = $data->year;
+	$role = $data->role;
+	$level = $data->level;
+	$organ = $data->organ;
+
 
 	//sql  query
-	$query = sprintf("UPDATE discipline SET name = '%s' WHERE idDiscipline=%d",
-		mysql_real_escape_string($name),
-		mysql_real_escape_string($id));
+	$query = sprintf("UPDATE discipline SET idInstitution = '%d',
+	year = '%d', role = '%s' , level = '%s' , organ = '%s'  WHERE idExam=%d",
+		mysql_real_escape_string($idInstitution),
+		mysql_real_escape_string($year),
+		mysql_real_escape_string($role),
+		mysql_real_escape_string($level),
+		mysql_real_escape_string($organ),
+		mysql_real_escape_string($idExam));
 
 	$rs = mysql_query($query);
 
 	echo json_encode(array(
 		"success" => mysql_errno() == 0,
 		"data" => array(
-			"idDiscipline" => $id,
-			"name" => $name)
+			"idExam" => $idExam,
+			"idInstitution" => $idInstitution,
+			"year" => $year,
+			"role" => $role,
+			"level" => $level,
+			"organ" => $organ)
 	  ));
