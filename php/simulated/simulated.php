3174699e99dc39d88836c4c40898f6e070d1b3e8
samueltcsantos <thiagsamuel@gmail.com>
2014-09-27 23:33:00 -0300

    criados o arquivo php para consultas randomicas na base de dados, issue #7

---
+++ php/simulated/simulated.php
@@ -0,0 +1,29 @@
+<?php
+
+ include('../connect.php');
+ 
+ $start = $_REQUEST['start'];
+ $limit = $_REQUEST['limit'];
+
+	$queryString = "SELECT * FROM question ORDER BY RAND() LIMIT $start,  $limit ";
+
+	//execute sql query
+	$query = mysql_query($queryString) or die(mysql_error());
+
+	//faz um looping e cria um array com os campos da consulta
+	$questions = array();
+	while($question = mysql_fetch_assoc($query)) {
+	    $questions[] = $question;
+	}
+
+	//consulta total de linhas na tabela
+	$queryTotal = mysql_query('SELECT count(*) as num FROM question') or die(mysql_error());
+	$row = mysql_fetch_assoc($queryTotal);
+	$total = $row['num'];
+
+	//encoda para formato JSON
+	echo json_encode(array(
+		"success" => mysql_errno() == 0,
+		"total" => $total,
+		"data" => $questions
+	));
