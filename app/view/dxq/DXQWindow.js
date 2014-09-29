b7a7f303dd1e71f1776a799a19caf2ff1784de35
samueltcsantos <thiagsamuel@gmail.com>
2014-09-29 11:03:46 -0300

    criada a janela para issue #8.

--- app/view/dxq/DXQWindow.js
+++ app/view/dxq/DXQWindow.js
@@ -1,1 +1,49 @@
-dd
+Ext.define('SysContest.view.dxq.DXQWindow',{
+		extend : 'Ext.window.Window',
+		alias  : 'widget.dxqwindow',
+		heigth : 100,
+		width  : 400,
+		modal  : true,
+		layout : 'fit',
+	 iconCls : 'icon-dqx',
+	 title : 'Disciplina X Questão',
+		autoShow : true,
+
+		items : [
+				{
+					xtype : 'panel',
+					defaults : {
+						anchor : '100%'
+					},
+					items : [
+						{
+							// xtype : 'simulatedpanel',
+						}
+					]
+				}
+		],
+
+		dockedItems : [
+			{
+				xtype : 'toolbar',
+				dock : 'bottom',
+				layout : {
+					type : 'hbox',
+					pack : 'end'
+				},
+				
+			  items: [
+			   {
+					 xtype : 'button',
+					 text : 'Cancelar',
+					 itemId : 'cancelDXQ',
+					 iconCls : 'icon-remove'
+			  	},
+				 { 
+					xtype : 'button',
+					text : 'Salvar',
+					itemId : 'saveDXQ',
+					iconCls : 'icon-save'
+				}]
+		}]
+});
