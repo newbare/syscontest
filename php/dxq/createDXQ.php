08908a81dc527589759c50eb3afb68238cc8c524
samueltcsantos <thiagsamuel@gmail.com>
2014-09-29 20:26:23 -0300

    salvando relacoes na base de dados, issue #8.

--- php/dxq/createDXQ.php
+++ php/dxq/createDXQ.php
@@ -4,20 +4,21 @@
 
 	$info = $_POST['data'];
 
-	//$data = json_decode(stripslashes($info));
 	$data = json_decode($info);
 
-	$name = $data->name;
+	$idDiscipline = $data->idDiscipline;
+	$idQuestion = $data->idQuestion;
 	
 	//sql query
-	$query = sprintf("INSERT INTO institution (name) values ('%s')",
-		mysql_real_escape_string($name));
+	$query = sprintf("INSERT INTO discipline_question values ('%d','%d')",
+		mysql_real_escape_string($idDiscipline),
+		mysql_real_escape_string($idQuestion));
 
 	$rs = mysql_query($query);
 
 	echo json_encode(array(
 		"success" => mysql_errno() == 0,
 		"data" => array(
-			"idInstitution" => mysql_insert_id(),
-			"name" => $name)
+			"idDiscipline" => $idDiscipline,
+			"idQuestion" => $idQuestion)
 	));
