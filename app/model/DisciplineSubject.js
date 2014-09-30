ee2ac45f0b025f7aaa2ae64ae0b375ad1ac943b7
samueltcsantos <thiagsamuel@gmail.com>
2014-09-30 18:39:23 -0300

    criado o Model DisciplineSubject, issue #8.

---
+++ app/model/DisciplineSubject.js
@@ -0,0 +1,10 @@
+Ext.define('SysContest.model.DisciplineSubject', {
+	extend : 'Ext.data.Model',
+
+	fields : [
+	  {name : 'idDXS', type : 'int'},
+			{name : 'idDiscipline', type : 'int'},
+			{name : 'idSubject' , type : 'int'}
+	]
+
+});
